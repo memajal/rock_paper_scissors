5a570c9759ee49e0e90a7645a0b0458fd393177e
memajal <memajalketa92@gmail.com>
2024-05-31 22:43:41 +0200

    Adding the style.js code of style to the and of index.js file
    Creating a div inside container and adding to other divs of human_choise and computer_choise to these div

--- index.js
+++ index.js
@@ -63,3 +63,28 @@ function playGame(humanChoise){
         }
         
 }
+
+
+/* index.html style with javascript */
+
+const container = document.querySelector("#container");
+const containerH1= container.querySelector("h1");
+containerH1.setAttribute("style", "text-align:center; color: blue;");
+
+const containerP= container.querySelector("p");
+containerP.setAttribute("style", "text-align:center; font-size: 20px; width:80%; margin:auto 10%; font-style:italic;");
+
+/*new div that will contain the computer choise and human choise divs */
+const choisesDiv = document.createElement("div");
+choisesDiv.id="choises";
+container.appendChild(choisesDiv);
+
+const computerChoiseDiv=document.createElement("div");
+computerChoiseDiv.id="computer_choise";
+choisesDiv.appendChild(computerChoiseDiv);
+
+const humanChoiseDiv=document.createElement("div");
+humanChoiseDiv.id="human_choise";
+choisesDiv.appendChild(humanChoiseDiv);
+
+
--- style.js
+++
@@ -1,9 +0,0 @@
-
-const container = document.querySelector("#container");
-const containerH1= container.querySelector("h1");
-containerH1.setAttribute("style", "text-align:center; color: blue;");
-
-const containerP= container.querySelector("p");
-containerP.setAttribute("style", "text-align:center; font-size: 20px; width:80%; margin:auto 10%; font-style:italic;");
-
-
